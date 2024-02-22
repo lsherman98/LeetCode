3c68499e889fb3eee90803e8570865f16dce5fff
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-02-22 12:13:22 -0500

    Time: 421 ms (86.96%), Space: 25.9 MB (84.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        counts = {}
+        for num in nums: 
+            if num in counts:
+                return True
+            else:
+                counts[num] = 1
+        return False
+
+                
+        
