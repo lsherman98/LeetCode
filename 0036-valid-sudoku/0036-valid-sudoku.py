a2528cfe8b766c09241dda5ca1a5ba1e4791409e
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-03-26 18:59:16 -0400

    Time: 85 ms (90.10%), Space: 16.6 MB (64.03%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,15 @@
-class Solution(object):
-     def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
         sqrs = collections.defaultdict(set)
-
+        
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.':
                     continue
+                
                 if (board[r][c] in rows[r] or 
                     board[r][c] in cols[c] or 
                     board[r][c] in sqrs[(r // 3, c // 3)]):
@@ -20,8 +18,5 @@ class Solution(object):
                 cols[c].add(board[r][c])
                 sqrs[(r // 3, c // 3)].add(board[r][c])
         return True
-
-
-
                 
-        
+            
