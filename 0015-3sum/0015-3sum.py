0bd1c3551d04d6504ccface16c42b759e593d62e
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-03-26 14:09:32 -0400

    Time: 636 ms (77.26%), Space: 20.8 MB (50.18%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,22 +3,23 @@ class Solution:
         res = []
         nums.sort()
         
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
                 continue
-            
+                
             l, r = i + 1, len(nums) - 1
             while l < r:
-                sum = a + nums[l] + nums[r]
-                if sum > 0:
+                threeSum = n + nums[l] + nums[r]
+                if threeSum > 0:
                     r -= 1
-                elif sum < 0:
+                elif threeSum < 0:
                     l += 1
                 else: 
-                    res.append([a, nums[l], nums[r]])
+                    res.append([n, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l < r:
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        return res
                         
+        return res
                 
