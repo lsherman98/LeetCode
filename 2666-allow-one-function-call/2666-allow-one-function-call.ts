6d372f2e4ded49dcfee881d3efb9998647d20bd6
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-03-27 20:26:41 -0400

    Time: 61 ms (34.81%), Space: 51.4 MB (31.85%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.ts
@@ -0,0 +1,23 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let count = 0
+    return function (...args) {
+        
+        if (count < 1) {
+            count++
+            return fn(...args)
+        } else {
+            return undefined
+        }
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
