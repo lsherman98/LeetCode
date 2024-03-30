96ad24b388f57b61de1af43dee27644e25014b66
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-03-29 23:14:42 -0400

    Time: 61 ms (50.04%), Space: 49.2 MB (79.31%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -6,12 +6,25 @@
 var timeLimit = function(fn, t) {
     
     return async function(...args) {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const id = setTimeout(() => reject('Time Limit Exceeded'), t)
-            fn(...args)
-                .then((res) => resolve(res))
-                .catch((err) => reject(err))
-                .finally(() => clearTimeout(id))
+            
+            try {
+                const res = await fn(...args)
+                resolve(res)
+            } catch(err) {
+                reject(err)
+            }
+            
+            
+      
+            
+            
+            
+            // fn(...args)
+            //     .then((res) => resolve(res))
+            //     .catch((err) => reject(err))
+            //     .finally(() => clearTimeout(id))
         })
     }
 };
