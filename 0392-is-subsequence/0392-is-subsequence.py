abe60d9d612fa05be870000c5cd5909239c63ebb
Levi Sherman <122409681+lsherman98@users.noreply.github.com>
2024-04-06 21:47:55 -0400

    Time: 34 ms (71.41%), Space: 16.6 MB (89.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,9 +5,7 @@ class Solution:
         while p1 < len(s) and p2 < len(t):
             if t[p2] == s[p1]:
                 p1 += 1
-                p2 += 1
-            else: 
-                p2 += 1
+            p2 += 1
                 
         return p1 == len(s)
         
